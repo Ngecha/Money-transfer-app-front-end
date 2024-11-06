91c0a529af2781bd34fc129325cee060bf799a4d
Ngecha <ngechadennis@gmail.com>
2024-11-06 22:45:38 +0300

    original commit

---
+++ client/money-transfer-app/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          Welcome to Money Transfer app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
